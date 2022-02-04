65c29d5251a9093e4ccf1137031909869181d2c2
iiriis <55133414+iiriis@users.noreply.github.com>
2022-02-05 04:20:22 +0530

    Introduced RTOS to handle tasks

--- SICPI/SICPI.ino
+++ SICPI/SICPI.ino
@@ -6,28 +6,40 @@ DHTStable DHT;
 #define DHT11_PIN 27
 #define mq2 14
 #define soilm 26
-
 #define bulb 13
 #define pump 12
+
+
+
+
+
+TaskHandle_t task1;
+
+
 
 
 double temp = 0.0, humid = 0.0, soilraw = 0.0, mqraw = 0.0;
 
 
 void setup() {
-  for (;;)
-  { 
+  Serial.begin(115200);
 
-    readSensors();
+  xTaskCreatePinnedToCore(readSensors, "Task1", 10000, NULL, 1, &task1, 0);
+
+
+
+  for (;;)
+  {
+
     Serial.print(humid);
     Serial.print(" ");
     Serial.print(temp);
     Serial.print(" ");
     Serial.print(mqraw);
     Serial.print(" ");
-    Serial.println(soilraw);  
+    Serial.println(soilraw);
 
-  }  
+  }
 
 }
 
@@ -36,14 +48,18 @@ void loop() {
 }
 
 
-void readSensors()
+void readSensors(void *pv)
 {
-  if (!DHT.read11(DHT11_PIN))
+  for (;;)
   {
-    temp = DHT.getTemperature();
-    humid = DHT.getHumidity();
-  }
+    if (!DHT.read11(DHT11_PIN))
+    {
+      temp = DHT.getTemperature();
+      humid = DHT.getHumidity();
+    }
 
-  mqraw=analogRead(mq2);
-  soilraw=analogRead(soilm); 
+    mqraw = analogRead(mq2);
+    soilraw = analogRead(soilm);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
 }
