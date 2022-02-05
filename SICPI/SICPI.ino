80e48cc840086baf0f9a70b2fb05116314bc0e85
iiriis <55133414+iiriis@users.noreply.github.com>
2022-02-05 05:44:48 +0530

    Added the MQTT broker

--- SICPI/SICPI.ino
+++ SICPI/SICPI.ino
@@ -1,7 +1,7 @@
 #include "DHTStable.h"
 #include <Wire.h>
-
-DHTStable DHT;
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 #define DHT11_PIN 27
 #define mq2 14
@@ -9,28 +9,101 @@ DHTStable DHT;
 #define bulb 13
 #define pump 12
 
-
-
-
+DHTStable DHT;
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
 
 TaskHandle_t task1;
 
-
+const char *ssid = "Dropkick";
+const char *pswd = "avijitdasxp";
+char *mqttServer = "saas.theakiro.com";
+int mqttPort = 1883;
 
 
 double temp = 0.0, humid = 0.0, soilraw = 0.0, mqraw = 0.0;
-
+long ls=0;
 
 void setup() {
   Serial.begin(115200);
 
+  connectToWiFi();
+  setupMQTT();
+  if (!mqttClient.connected())
+  connectMQTT();
+  mqttClient.subscribe("Room");
+  
   xTaskCreatePinnedToCore(readSensors, "Task1", 10000, NULL, 1, &task1, 0);
 
-
+String t="";
+char buf[10];
 
   for (;;)
   {
+    
+    if(millis()-ls>1000)
+    {
+    t=String(humid) + String(temp)+ String(soilraw)+ String(mqraw);
+    t.toCharArray(buf,20);
+    mqttClient.publish("sicpi", buf);
+    ls=millis();
+    }
 
+    if (!mqttClient.connected())
+    connectMQTT();
+    mqttClient.loop();
+  }
+
+}
+
+void loop()
+{
+
+}
+
+void connectToWiFi()
+{
+  Serial.print("Connecting to ");
+
+  WiFi.begin(ssid, pswd);
+  Serial.println(ssid);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("Connected to WiFi.");
+}
+
+void setupMQTT()
+{
+  mqttClient.setServer(mqttServer, mqttPort);
+  mqttClient.setCallback(callback);
+}
+
+void connectMQTT() 
+{
+  Serial.println("Connecting to Akiro...");
+  while (!mqttClient.connected()) 
+  {
+    String clientId = "sicpi";
+    if (mqttClient.connect(clientId.c_str(), "qWdhxyJzxnZY:@sicpi", "sicpi123" ))
+      Serial.println("Connected to Akiro.");
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  Serial.print("Callback - ");
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) 
+    Serial.print((char)payload[i]);
+
+  Serial.println();
+}
+
+void printSensors()
+{
     Serial.print(humid);
     Serial.print(" ");
     Serial.print(temp);
@@ -38,15 +111,7 @@ void setup() {
     Serial.print(mqraw);
     Serial.print(" ");
     Serial.println(soilraw);
-
-  }
-
 }
-
-void loop() {
-
-}
-
 
 void readSensors(void *pv)
 {
